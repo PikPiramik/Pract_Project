6d34887c6d85d48579851f2272e281e7159d715a
PikPiramik <102436125+PikPiramik@users.noreply.github.com>
2025-07-20 20:39:45 +0300

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key'
-    SCAN_RESULTS_DIR = 'reports'
-    ALLOWED_TARGETS = ['example.com', '192.168.1.0/24']  # Разрешённые цели
